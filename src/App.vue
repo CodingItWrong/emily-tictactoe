d92530c07302281f366004bc8c9a107a8723d74f
Josh Justice <josh.justice@accesscfa.com>
2019-11-10 11:06:53 -0500

    Add title

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>The Best Tic-Tac-Toe Ever</h1>
+    <h2>Made By Emily</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
 };
 </script>
 
