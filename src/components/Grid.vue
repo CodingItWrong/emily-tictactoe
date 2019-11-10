a64ad808af5e1075cd326dfb25f55aba7fca19c0
Josh Justice <josh.justice@accesscfa.com>
2019-11-10 11:35:22 -0500

    Indicate turn

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div>{{turnShape}}'s turn</div>
     <div v-for="(row, rowIndex) in rows" :key="rowIndex">
       <Square
         v-for="(space, columnIndex) in row"
@@ -28,6 +29,7 @@ export default {
         [0, 0, 0],
       ],
       shapes: ['_', 'X', 'O'],
+      turn: 1,
     }
   },
   methods: {
@@ -41,6 +43,9 @@ export default {
   computed: {
     shape(shapeIndex) {
       return this.shapes[shapeIndex]
+    },
+    turnShape() {
+      return this.shapes[this.turn]
     },
   },
 }
